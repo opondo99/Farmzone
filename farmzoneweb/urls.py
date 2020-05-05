cbfcdfbb3f038ae40195673f0da77c63ed99d26f
dabusharsystems <patogee@gmail.com>
2020-05-05 23:50:48 +0300

    Template URL
    Template url config pt 2

--- farmzoneweb/urls.py
+++ farmzoneweb/urls.py
@@ -5,12 +5,12 @@ from . import views
 app_name = 'farmzoneweb'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index.html', views.index, name='index'),
     path('about.html', views.about, name='about'),
     path('contact.html', views.contact, name='contact'),
     path('forum.html', views.forum, name='forum'),
-    path('listing.html', views.listings, name='marketplace'),
-    path('listing-single.html', views.listing_single, name='Ad page'),
+    path('listings.html', views.listings, name='marketplace'),
+    path('listings-single.html', views.listing_single, name='Ad page'),
     path('login.html', views.login, name='Login'),
     path('postitem.html', views.post_item, name='post item'),
     path('profile.html', views.profile, name='profile'),
