b25aefb2f2708192600d14d2b5da3edf2e3d78fa
Stephen-Kamau <stiveckamash@gmail.com>
2020-05-13 23:15:15 -0400

    fixed style issues

--- farmzoneweb/models.py
+++ farmzoneweb/models.py
@@ -10,16 +10,19 @@ import uuid
 
 class Thread(models.Model):
     '''
-    This is the starter for the thread
+    The starter for the thread
     Once it has been created  by a user,,
     Other people can comment about it
     '''
     thread_id = models.UUIDField(primary_key = True , unique = True , default=uuid.uuid4)
-    user_id = models.ForeignKey(User , on_delete = models.CASCADE)
+    # user_id = models.ForeignKey(User , on_delete = models.CASCADE)
     thread_message = models.TextField(max_length = 1255)
     thread_post_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
+        '''
+        name for the table
+        '''
         db_table = "Thread"
 
     def _str__(self):
@@ -28,14 +31,14 @@ class Thread(models.Model):
 
 class Forum(models.Model):
     '''
-    This class create db for the forum messaging details
+    The class to create db for the forum messaging details
     with the time of post
     poster who posted
     and the messages itself
     This will be a reply from # thread table
     '''
     forum_id = models.UUIDField(primary_key = True , unique = True , default=uuid.uuid4)
-    user_id = models.ForeignKey(User,on_delete = models.CASCADE)#this is not indicated in the schema
+    # user_id = models.ForeignKey(User,on_delete = models.CASCADE)#this is not indicated in the schema
     forum_post = models.TextField(max_length=1255)
     forum_post_time = models.DateTimeField(auto_now_add=True)
     thread_id = models.ForeignKey(Thread , on_delete = models.CASCADE)
@@ -44,6 +47,9 @@ class Forum(models.Model):
 
 
     class Meta:
+        '''
+        name for the table
+        '''
         db_table = "Forum"
 
     def __str__(self):
@@ -54,11 +60,11 @@ class Forum(models.Model):
 #chat table for commenting about a product
 class Chat(models.Model):
     '''
-    This is implimentation for the caht table in which a user can comment
+    The implimentation for the caht table in which a user can comment
     about the product
     '''
     chat_id = models.UUIDField(default = uuid.uuid4 , primary_key = True , unique = True)
-    user_id = models.ForeignKey(User , on_delete = models.CASCADE)
+    # user_id = models.ForeignKey(User , on_delete = models.CASCADE)
     message = models.TextField(max_length = 1255)
     # username .....my thougth is we have user_id we can the get the username using it
     #i aslo think we can get the time---- in full format instead of getting time and date separetely
@@ -66,13 +72,15 @@ class Chat(models.Model):
     message_post_time = models.DateTimeField(auto_now_add = True)
     #i have added ads-id since am seeing its purposes for showing which commodity was
     # being commented about
-    ads_id = models.ForeignKey(ProductsAds , on_delete = models.CASCADE)
-    category_id = models.ForeignKey(Category , on_delete = models.CASCADE)
-    #quite not sure of purpose for thread id here
+    # ads_id = models.ForeignKey(ProductsAds , on_delete = models.CASCADE)
+    # category_id = models.ForeignKey(Category , on_delete = models.CASCADE)
 
 
 
     class Meta():
+        '''
+        name for the table
+        '''
         db_table = 'Chat'
 
 
