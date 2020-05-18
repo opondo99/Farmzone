6164b2be86cf53daab3af5bd544a37dd5dd6ce21
opondo99 <38689584+opondo99@users.noreply.github.com>
2020-05-18 09:54:00 +0300

    update urls
    Added urls to be used in template linking

--- farmzoneweb/urls.py
+++ farmzoneweb/urls.py
@@ -7,15 +7,15 @@ app_name = 'farmzoneweb'
 urlpatterns = [
     path('', views.index, name='index'),
     path('index.html', views.index, name='index'),
-    # #path('admin')
-    # path('about.html', views.about, name='about'),
-    # path('contact.html', views.contact, name='contact'),
-    # path('forum.html', views.forum, name='forum'),
-    # path('listings.html', views.listings, name='marketplace'),
-    # path('listings-single.html', views.listing_single, name='Ad page'),
-    # path('login.html', views.login, name='Login'),
-    # path('postitem.html', views.post_item, name='post item'),
-    # path('profile.html', views.profile, name='profile'),
-    # path('register.html', views.register, name='register'),
+    #path('admin')
+    path('about.html', views.about, name='about'),
+    path('contact.html', views.contact, name='contact'),
+    path('forum.html', views.forum, name='forum'),
+    path('listings.html', views.listings, name='marketplace'),
+    path('listings-single.html', views.listing_single, name='ad-page'),
+    path('login.html', views.login, name='Login'),
+    path('postitem.html', views.post_item, name='post-item'),
+    path('profile.html', views.profile, name='profile'),
+    path('register.html', views.register, name='register'),
 
 ]
